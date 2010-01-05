95d8cd1b92a6217283fa0ca41da892a56913d762
Olexiy Zamkoviy <olexiy.z@gmail.com>
2010-01-05 18:03:12 +0200

    Removed useless code from map_controller

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,6 +1,2 @@
 class MapController < ApplicationController
-
-  def flash
-    render :text => File.open(RAILS_ROOT + '/public/images/jquery.clipboard.swf').read, :content_type => "application/x-shockwave-flash"
-  end
 end
