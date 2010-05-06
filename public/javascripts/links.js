4ffeb34052ff7ae360edecebe64f5eecb9e901fb
Olexiy Zamkoviy <olexiy.z@gmail.com>
2010-05-06 11:43:44 +0300

    Refactored links page

---
+++ public/javascripts/links.js
@@ -0,0 +1,48 @@
+$(function(){
+  $('#addLink').jqm({
+    modal: true,
+    trigger: '.jqm',
+    onShow: function(el){
+      $('.message').remove();
+      $('.error').remove();
+      el.w.show();
+    }
+  });
+});
+
+(function(saveButton, form, url){
+  $(saveButton).click(function(){
+    var form = $(form);
+    form.ajaxSubmit({
+      type : 'POST',
+      dataType : 'json',
+      url : url,
+      semantic: true,
+      success : function(result){
+        if(result.success){
+          $('#addLink').jqmHide();
+          $.scrollTo('body', {
+            duration: 500, 
+            onAfter: function(){
+              $('.soria').prepend('<span class="message">Успішно додано, після перевірки адміністратором посилання потрапить на цю сторінку</span>');
+              form.clearForm();
+            }
+          });
+        }else{
+          var errors = result;
+          for (var i = 0; i < errors.length; i++) {
+            $('#link_' + errors[i][0]).parent().siblings('.for_error').append(
+              '<span class="error">' + errors[i][1] + '</span>'
+            );
+          };
+        }
+      },
+      failure: function(){
+        alert('Виникла помилка зв\'язку з сервером')
+      },
+      beforeSubmit : function(){
+        $('.error').remove();
+      }
+    });
+  });
+})('.save', '#jqmForm', '#{add_url}');
